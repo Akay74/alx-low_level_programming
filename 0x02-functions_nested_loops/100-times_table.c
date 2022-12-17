5791f0c761e7c319576d07de989b0739184f1092
Akay Udeh <akaudeh@gmail.com>
2022-12-17 12:50:17 -0800

    times table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -27,7 +27,8 @@ void print_times_table(int n)
 					_putchar(' ');
 					_putchar(' ');
 					_putchar(' ');
-					_putchar('0' + prod);												} else if (prod > 99)
+					_putchar('0' + prod);
+				} else if (prod > 99)
 				{
 					_putchar(',');
 					_putchar(' ');
